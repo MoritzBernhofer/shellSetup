4deda1d85d1270ec35bb196918fafaa648535e87
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2025-04-11 12:37:19 +0200

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,22 +1,35 @@
 clear
 
 autoload -U compinit && compinit
+
+# paths
+
 path+=('/opt/homebrew/bin')
 path+=('/Users/moritz/Library/Python/3.9/bin')
-path+=('/opt/homebrew/opt/ruby/bin/ruby')
 export PATH="$PATH:/Users/moritz/.dotnet/tools"
 
+# quality of life
 alias cd..="cd .."
-alias tsc="npx tsc"
 alias ls="lsd -lah"
-alias run="~/customprograms/./run.sh"
+
+# customscripts
 alias killp="~/customprograms/./killp.sh"
-alias pcp='pwd | pbcopy'
-alias dps='docker ps -la'
+alias run="~/customprograms/./run.sh"
+
+# kubernetes
 alias kub='kubectl'
+
+# paths
 alias cdtmp='cd ~/Desktop/tmp/'
+alias startup='nvim ~/Documents/GitHub/shellSetup/startup.sh'
+alias pcp='pwd | pbcopy'
+
+#docker
 alias dcb='docker compose build'
 alias dcu='docker compose up'
+alias dps='docker ps -la'
+
+#dotnet
 alias addMigration='dotnet ef migrations add Initial'
 alias applyMigration='dotnet ef database update'
 
